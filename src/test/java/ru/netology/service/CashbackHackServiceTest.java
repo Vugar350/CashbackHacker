478d9786bac6fbc3e9fd471a49d8c737e2affa8f
vugar350 <Vugar350@mail.ru>
2022-02-12 08:20:40 +0300

    initial commit

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -38,7 +38,7 @@ public class CashbackHackServiceTest {
         CashbackHackService service = new CashbackHackService();
         int amount;
         int actual=service.remain(1000);
-        int expected=1000;
+        int expected=0;
         assertEquals (actual,expected);
 
     }
